fbf1df770e731ec6d224819461988132c68b5215
thebjko <thebjko@gmail.com>
2023-01-07 14:14:04 +0900

    Update 11021, 15552

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -2,5 +2,4 @@ import sys
 
 nums = sys.stdin.readlines()
 for i, j in enumerate(nums[1:]):
-    print(f'Case #{i+1}:',sum(map(int, j.split())))
-    
+    print(f'Case #{i+1}:', sum(map(int, j.split())))
--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -5,3 +5,8 @@ nums = open(0).readlines()
 for i in nums[1:]:
     a,b=i.split()
     print(add(int(a),int(b)))
+
+"""
+숏코딩
+for l in[*open(0)][1:]:print(sum(map(int,l.split())))
+"""
