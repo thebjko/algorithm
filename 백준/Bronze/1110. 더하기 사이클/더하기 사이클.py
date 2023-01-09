1fda43aafbe2027fa0c967f5d375dbabcef19b8d
Bongjin Ko <thebjko@gmail.com>
2023-01-09 09:06:27 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,12 +1,14 @@
 num = [open(0).read().strip()]
 
-if len(num[0]) == 1:
-    num[0] = '0' + num[0]
-
-for i, _ in enumerate(num := num):
+i = 0
+while (num := num):
     n = num[i]
+    if len(n) > 1:
+        a, b = n[0], n[1]
+    else:
+        num[i] = '0' + num[i]
+        a, b = '0', n[-1]
     
-    a, b = n[0], n[1]
     c = str(int(a) + int(b))
     d = c[-1]
     e = b + d
@@ -16,10 +18,3 @@ for i, _ in enumerate(num := num):
     if num[-1] == num[0]:
         print(len(num)-1)
         break
-
-"""
-숏코딩
-a=n=int(input());c=1
-while(a:=a%10*10+a*11//10%10)-n:c+=1
-print(c)
-"""
