dc4e48ca1c212a551a00b9f5c659e5f340e26ab0
thebjko <thebjko@gmail.com>
2023-01-10 21:08:22 +0900

    reset 한수.py by one commit

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -6,18 +6,18 @@ result = 0
 if i < 100:
     print(i)
 else:
-    for n in range(100, i):
+    for n in range(100, i+1):
         while n:
             ls.append(n%10)
             n //= 10
         if ls[0]-ls[1] == ls[1]-ls[2]:
-            # try:
-            #     if ls[1]-ls[2] == ls[2]-ls[3]:
-            #         result += 1
-            # except:
-            result += 1
-            # finally:
-            ls = []
+            try:
+                if ls[1]-ls[2] == ls[2]-ls[3]:
+                    result += 1
+            except:
+                result += 1
+            finally:
+                ls = []
         else:
             ls = []
 
