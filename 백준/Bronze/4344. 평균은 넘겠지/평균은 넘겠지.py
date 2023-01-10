64721dc851018b2876cbeffbf734a017af514ce0
thebjko <thebjko@gmail.com>
2023-01-10 15:49:06 +0900

    update 평균은 넘겠지.py 숏코딩

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -9,3 +9,11 @@ while c:
             o += 1
     print(f'{o/len(ls[1:])*100:.3f}%')
     c -= 1
+
+"""
+숏코딩
+for i in[*open(0)][1:]:
+    a,*b=map(int,i.split())
+    print(f'{sum(a*j>sum(b)for j in b)/a:.3%}')
+    
+"""
