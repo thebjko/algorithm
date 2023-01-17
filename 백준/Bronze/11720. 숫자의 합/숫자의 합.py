4fb3655ec4ab6f8e3111c9c579bdc018b25eda28
Bongjin Ko <thebjko@gmail.com>
2023-01-17 14:36:56 +0900

    [Bronze IV] Title: 숫자의 합, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,11 +1,2 @@
-T = open(0).read().split()
-
-n = 0
-for i in T[1]:
-    n += int(i)
-
-print(n)
-
-"""
-print(*map(int, '12353246'))   # 1 2 3 5 3 2 4 6
-"""
+_, a = open(0)
+print(sum(int(i) for i in a.strip()))
