6bad2c0e5346961cd4455d7efb41034bc7592889
thebjko <thebjko@gmail.com>
2023-01-18 19:27:22 +0900

    sc

--- scratch.py
+++ scratch.py
@@ -1,24 +1,4 @@
-# import time
-
-T = int(input())
-# start_time = time.time()
-n = 1
-
-flag = 1
-numerator = [1]
-denominator = [1]
-while T > n:
-    T -= n
-    n += 1
-    
-    ls = [*range(1, n+1)]
-    numerator = ls[::flag]
-    flag *= -1
-    denominator = ls[::flag]
-    print(denominator, numerator, n-T)
-
-print(f"{denominator[n - T]}/{numerator[n - T]}")
-# end_time = time.time()
-# print(end_time - start_time)
-
-
+from math import ceil
+a, b, v = map(int, open(0).read().split())
+# a + (a - b) * x = v
+print(ceil((v - a)/(a - b)) + 1)
