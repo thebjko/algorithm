dab5ae9a9ef1926465487672d61041b06dcd6feb
Bongjin Ko <thebjko@gmail.com>
2023-01-18 09:17:29 +0900

    [Bronze I] Title: 분수찾기, Time: 308 ms, Memory: 270224 KB -BaekjoonHub

---
+++ 백준/Bronze/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,27 @@
+# import time
+
+T = int(input())
+# start_time = time.time()
+n = 1
+
+denominator = [[1]]
+numerator = [[1]]
+
+flag = 1
+
+while True:
+    if T > n:
+        T -= n
+        n += 1
+        
+        ls = [*range(1, n + 1)]
+        numerator.append(ls[::flag])
+        flag *= -1
+        denominator.append(ls[::flag])
+
+    else:
+        print(f"{denominator[n - 1][n - T]}/{numerator[n - 1][n - T]}")
+        # end_time = time.time()
+        # print(end_time - start_time)
+        break
+
