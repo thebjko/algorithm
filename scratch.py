0652910b0ba47d6bcbf3974d6a71356324b97e2d
thebjko <thebjko@gmail.com>
2023-01-18 19:36:35 +0900

    sc

--- scratch.py
+++ scratch.py
@@ -2,3 +2,7 @@ from math import ceil
 a, b, v = map(int, open(0).read().split())
 # a + (a - b) * x = v
 print(ceil((v - a)/(a - b)) + 1)
+print(1-(a-v)//(a-b))
+print(1+(v-a)//(a-b))
+print(1-(a-v)/(a-b))
+print(1+(v-a)/(a-b))
