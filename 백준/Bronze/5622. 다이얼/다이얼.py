eab4623d7b826f262e11514dff176669eafd4084
Bongjin Ko <thebjko@gmail.com>
2023-01-19 13:56:21 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,25 +1,7 @@
-chars = list(map(chr, range(65,91)))
-ls = list(map(lambda x: (x-56)//3, range(65, 91)))
-ls[chars.index('S')] -= 1
-ls[ls.index(10)] -= 1
-ls[ls.index(11)] -= 1
-ls[ls.index(11)] -= 1
-
-s = 0
+ls = [3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10]
+alphabet = list(map(chr, range(65, 91)))
+n = 0
 for i in input():
-    s += ls[chars.index(i)]
+    n += ls[alphabet.index(i)]
 
-print(s)
-
-"""
-숏코딩 분석
-print(sum(5*min(ord(x),88)//16-17for x in input()))
-
-s = 0
-for x in input():
-    s += 5 * min(ord(x), 88) // 16 - 17)
-
-print(s)
-
-이건 뭐 별수없다.
-"""
+print(n)
