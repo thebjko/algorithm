11c25640679665faa304cc8a152a1199c481d082
Bongjin Ko <thebjko@gmail.com>
2023-01-26 11:38:40 +0900

    [Silver V] Title: 카드1, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,13 @@
+def card_1_2161():
+    n = int(input())
+    cards = list(range(1, n + 1))
+    while True:
+        print(cards.pop(0))
+        if cards:
+            cards.append(cards.pop(0))
+        else:
+            break
+
+if __name__ == "__main__":
+    solution = card_1_2161
+    solution()
