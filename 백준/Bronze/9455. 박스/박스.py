185c014003ec9d660c22d327d16e0694a584ef97
Bongjin Ko <thebjko@gmail.com>
2023-02-01 08:39:30 +0900

    [Bronze I] Title: 박스, Time: 192 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9455. 박스/박스.py
+++ 백준/Bronze/9455. 박스/박스.py
@@ -1,24 +1,20 @@
-# 박스
 import sys
+
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     matrix = []
-    rows, cols = map(int, input().split())
-    for r in range(rows):
-        matrix.append(list(map(int, input().split())))
+    rows, _ = map(int, input().split())
+    for _ in range(rows):
+        matrix.append(input().split())
     
-    matrix.reverse()
     rotated = list(map(list, zip(*matrix)))
 
     cnt = 0
     for i in rotated:
-        s = ''.join(map(str,i))
-        while True:
-            if "01" in s:
-                s = s.replace("01", "10", 1)
-                cnt += 1
-            else: 
-                break
+        s = ''.join(i)
+        while "10" in s:
+            cnt += s.count("10")
+            s = s.replace("10", "01")
 
     print(cnt)
