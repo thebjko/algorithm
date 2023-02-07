39cd5f208367bdcf84fcace20950028320cdc516
Bongjin Ko <thebjko@gmail.com>
2023-02-07 17:20:52 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,24 @@
+n, *ls = map(int, open(0).read().split())
+
+weight = ls[::2]
+stature = ls[1::2]
+
+min_weight, max_weight = min(weight), max(weight)
+min_stature, max_stature = min(stature), max(stature)
+
+matrix = [[0] * (max_weight - min_weight + 1) for _ in range(max_stature - min_stature + 1)]
+
+adj_weight = map(lambda x: x - min_weight, weight)
+adj_stature = map(lambda x: x - min_stature, stature)
+
+for w, s in zip(adj_weight, adj_stature):
+    matrix[s][w] = 1
+
+for i in range(0, 2 * n, 2):
+    w, s = ls[i] - min_weight, ls[i + 1] - min_stature
+    try:
+        rank = sum(map(lambda x: sum(x[w + 1:]), matrix[s + 1:]))
+    except:
+        rank = 1
+    
+    print(rank + 1, end=" ")
