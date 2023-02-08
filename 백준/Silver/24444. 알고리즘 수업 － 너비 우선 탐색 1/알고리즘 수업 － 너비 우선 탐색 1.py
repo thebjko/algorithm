e6345567bdb9c40ab9f34436df738c70fe0bf38c
Bongjin Ko <thebjko@gmail.com>
2023-02-08 19:09:04 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 840 ms, Memory: 91248 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,12 +1,12 @@
 import sys
-from queue import Queue
+from collections import deque
 
 input = sys.stdin.readline
 
 class Graph:
     def __init__(self) -> None:
         self.vertices: dict[int, set[int]] = {}
-        self.cnt: int = 2
+        self.cnt: int = 1
     
     def add_edge(self, from_vertex: int, to_vertex: int) -> None:
         if from_vertex in self.vertices:
@@ -15,16 +15,16 @@ class Graph:
             self.vertices[from_vertex] = set((to_vertex, ))
 
     def bfs(self, start_vertex: int) -> dict[int, int]:
-        queue: Queue = Queue()
-        visited = dict([(start_vertex, 1)])
-        queue.put(start_vertex)
-        while not queue.empty():
-            vertex = queue.get()
+        queue: deque = deque()
+        visited = dict([(start_vertex, self.cnt)])
+        queue.append(start_vertex)
+        while queue:
+            vertex = queue.popleft()
             for adj in sorted(self.vertices.get(vertex, set())):
                 if adj not in visited:
-                    visited.update([(adj, self.cnt)])
-                    queue.put(adj)
                     self.cnt += 1
+                    visited.update([(adj, self.cnt)])
+                    queue.append(adj)
         return visited
 
 if __name__ == '__main__':
