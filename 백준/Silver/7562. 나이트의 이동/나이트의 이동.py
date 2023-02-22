1a17ebfe6827acd83515b7da98351ccf4c227713
Bongjin Ko <thebjko@gmail.com>
2023-02-22 21:28:00 +0900

    [Silver I] Title: 나이트의 이동, Time: 2460 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -8,15 +8,19 @@ for _ in range(int(input())):
     board = [[0] * size for _ in range(size)]
     start = tuple(map(int, input().split()))
     end = tuple(map(int, input().split()))
+    if start == end:
+        print(0)
+        continue
     q = deque([start])
     while q:
         (x, y) = q.popleft()
-        if (x, y) == end:
-            print(board[y][x])
-            break
         for dx, dy in delta:
             a, b = x+dx, y+dy
             if not (0<=a<size and 0<=b<size) or board[b][a]:
                 continue
             board[b][a] = board[y][x] + 1
+            if (a, b) == end:
+                print(board[b][a])
+                q = 0
+                break
             q.append((a, b))
