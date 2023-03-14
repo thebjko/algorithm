473c6ac04d2205ea789034fae3e3e18ac62897bc
Bongjin Ko <thebjko@gmail.com>
2023-03-14 15:00:38 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.12 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,21 +1,42 @@
 from collections import defaultdict
+from itertools import product
 
-# 1번, 17번
+dates_dict = {
+    '.'.join(date): idx for idx, date in enumerate(
+        product(
+            [str(l) for l in range(2000, 2032)],
+            [str(m).zfill(2) for m in range(1, 13)],
+            [str(n).zfill(2) for n in range(1, 29)]
+        )
+    )
+}
+
+# 10, 12, 15, 18, 20
 def solution(today, terms, privacies):
     terms_dict = defaultdict(int)
-    for idx, term in enumerate(terms):
+    for term in terms:
         key, val = term.split()
         terms_dict[key] = int(val)
 
-    result = []
-    for idx, val in enumerate(privacies):
-        date, term = val.split()
-        date = list(map(int, date.split('.')))
-        date[1] += terms_dict[term]
-        date[0] += (date[1] - 1) // 12
-        date[1] -= ((date[1] - 1) // 12) * 12
-        date = '.'.join(map(lambda x: str(x).zfill(2), date))
-        if date <= today:
-            result.append(idx+1)
-        
-    return sorted(result)
+    privacies_dict = defaultdict(list)
+    for idx, term in enumerate(privacies):
+        privacies_dict[term].append(idx+1)
+
+    idx_today = dates_dict[today]    
+    
+    answer = []
+    for prv in set(privacies):
+        date, term = prv.split()
+        if dates_dict[date] + 28 * terms_dict[term] <= idx_today:
+            answer += privacies_dict[prv]
+
+    return sorted(answer)
+
+if __name__ == '__main__':
+    cases = [
+        ["2022.12.08", ["A 6"], ["2022.06.08 A"]],
+        ["2021.12.08", ["A 18"], ["2020.06.08 A"]],
+    ]
+
+    for c in cases:
+        print(solution(*c))
