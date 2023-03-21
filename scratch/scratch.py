3e4dd511c8d7051165d507336c702a19cf4a54c1
thebjko <thebjko@gmail.com>
2023-03-21 16:40:26 +0900

    -

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,21 +1,44 @@
 import io, os, sys
 from heapq import *
+from collections import defaultdict
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 # input = io.BytesIO(os.read('input.txt', os.fstat('input.txt').st_size)).readline
+
+def path_finder(x: int, path: list = []):
+    # global s
+    if x == s:
+        return path
+    
+    path.append(x)
+    candidate = 0
+    min_dist= 10**8
+    for key, d in graph.items():
+        if x in d:
+            total = distance[key] + d[x]
+            if total < min_dist:
+                min_dist = total
+                candidate = key
+    
+    if candidate:
+        return path_finder(candidate, path)
+
+    return []
 
 inf = float('inf')
 for _ in range(int(input())):
     n, m, t = map(int, input().split())   # 교차로(node), 도로(edge), 목적지 후보의 개수
     s, g, h = map(int, input().split())   # 출발지, g -> h 로 지나갔다.
     
-    graph = [[] for _ in range(n+1)]
+    graph = defaultdict(dict[int])
     distance = [inf] * (n+1)
     for _ in range(m):
         a, b, d = map(int, input().split())   # a <-> b, 거리 d
-        graph[a].append((b, d))
-        graph[b].append((a, d))
+        if {a, b} == {g, h}:
+            d -= .01
+        graph[a][b] = d
+        graph[b][a] = d
 
     # 데이크스트라
     q = [(0, s)]
@@ -24,19 +47,21 @@ for _ in range(int(input())):
         cost, current_node = heappop(q)
         if cost > distance[current_node]:   # 같으면 다른 경로를 선택할 수 있기 때문에 >=가 아니라 >
             continue
-        for node_to_proceed, cost_to_next_node in graph[current_node]:
-            if current_node in {g, h} and node_to_proceed not in {g, h}:
-                continue
+        for node_to_proceed, cost_to_next_node in graph[current_node].items():
             cost_to_proceed = cost + cost_to_next_node
             if cost_to_proceed < distance[node_to_proceed]:
                 distance[node_to_proceed] = cost_to_proceed
                 heappush(q, (cost_to_proceed, node_to_proceed))
 
-    result = []
+    result = set()
     for _ in range(t):
-        x = int(input())   # 목적지 후보
-        if distance[x] != inf:
-            result.append(distance[x])
+        x = int(input())
+        path = path_finder(x)
+        for idx, val in enumerate(path):
+            if val in {g, h}:
+                idx += 1
+                if idx < len(path):
+                    if path[idx] in {g, h}:
+                        result.add(x)
 
     print(*sorted(result))
-            
