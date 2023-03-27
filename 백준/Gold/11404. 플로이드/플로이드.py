59cdb287fe120bbde19fb5a7116116f23ece270d
Bongjin Ko <thebjko@gmail.com>
2023-03-27 14:34:45 +0900

    [Gold IV] Title: 플로이드, Time: 408 ms, Memory: 40304 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -12,15 +12,14 @@ for i in range(1, n+1):
 
 for i in ls:
     a, b, c = map(int, i.split())
-    matrix[a][b] = min(matrix[a][b], c)
+    if c < matrix[a][b]:
+        matrix[a][b] = c
 
 
 for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
-            matrix[a][b] = min(matrix[a][b], matrix[a][k] + matrix[k][b])
+            if matrix[a][b] > matrix[a][k] + matrix[k][b]:
+                matrix[a][b] = matrix[a][k] + matrix[k][b]
 
-
-for i in matrix[1:]:
-    for val in i[1:]:
-        print(0 if isinf(val) else val)
+print('\n'.join(' '.join('0' if isinf(val) else str(val) for val in i[1:]) for i in matrix[1:]))
