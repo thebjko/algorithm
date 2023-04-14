ecc45638c58c7899bb1727ad6e46b145ad52c3bf
Bongjin Ko <thebjko@gmail.com>
2023-04-14 21:17:29 +0900

    [unrated] Title: 요격 시스템, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(500_000)
+
+
+def solution(targets):
+    targets.sort()
+    q = deque(targets)
+
+    def f(q: deque, mark: int = 0, threshold: int = 0, answer: int = 0) -> int:
+        if not q:
+            return answer
+        e = mark
+        while q:
+            x, y = q.popleft()
+            if x >= e:
+                answer += 1
+                e = y
+            elif y <= e:
+                return f(q, y, e, answer)
+        return answer
+    return f(q)
